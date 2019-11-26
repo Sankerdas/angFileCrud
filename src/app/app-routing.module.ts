8183bfc1323068f9fde8e517b7b4bff7e995ac89
Sanker das <sankerdas.yes@gmail.com>
2019-11-26 14:16:31 -0800

    component added
    1 create, list userts component added
    2 basic ruouter and link added
    3 bootsrap installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserCreateComponent} from './user-create/user-create.component';
+import {UsersListComponent} from './users-list/users-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-user', component: UserCreateComponent },
+  { path: 'list-users', component: UsersListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'create-user' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCreateComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
