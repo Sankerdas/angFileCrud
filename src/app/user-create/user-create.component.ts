a4eefe723156e64bdcf3c8b8c6c68027d236a800
Sanker das <sankerdas.yes@gmail.com>
2019-11-30 16:46:48 -0800

    image upload ( image preview)
    image uploading form created,
    uploaded image patched to formgroup
    image preivewied ( using FileReader() )

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-user-create',
@@ -6,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent implements OnInit {
+  imagePreview: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  // Reactive form
+  form: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    avatar: [null, Validators.required]
+  });
+
+  fileUpload(e) {
+    // file adding to form
+   const uploadedFile = (e.target as HTMLInputElement).files[0]; // getting file
+   this.form.patchValue({ avatar:uploadedFile }); // patching form data and adding the file
+   this.form.get('avatar').updateValueAndValidity(); // check validity 
+
+   // Imager preview
+   const reader = new FileReader();
+   reader.onload = () => {
+     this.imagePreview = reader.result;
+   }
+   reader.readAsDataURL(uploadedFile);
+  }
 
   ngOnInit() {
   }
